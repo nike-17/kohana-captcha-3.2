ffb22c03b7783b097c72001b23fe53f7feed49cb
Nikita <nike-17@ya.ru>
2012-02-14 18:46:59 +0700

    fix 3.2 Request

--- classes/captcha.php
+++ classes/captcha.php
@@ -434,10 +434,11 @@ abstract class Captcha
 			return '<img src="'.url::site('captcha/'.Captcha::$config['group']).'" width="'.Captcha::$config['width'].'" height="'.Captcha::$config['height'].'" alt="Captcha" class="captcha" />';
 
 		// Send the correct HTTP header
-        Request::instance()->headers['Content-Type'] = 'image/'.$this->image_type;
-        Request::instance()->headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0';
-        Request::instance()->headers['Pragma'] = 'no-cache';
-        Request::instance()->headers['Connection'] = 'close';
+		
+        Request::initial()->headers['Content-Type'] = 'image/'.$this->image_type;
+        Request::initial()->headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0';
+        Request::initial()->headers['Pragma'] = 'no-cache';
+        Request::initial()->headers['Connection'] = 'close';
 
 		// Pick the correct output function
 		$function = 'image'.$this->image_type;
